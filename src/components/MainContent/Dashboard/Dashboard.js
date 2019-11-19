0205d8d4a31d09a068a819da64924e257bd4b33d
Lilit Poghosyan <lilitpoghosyan94@gmail.com>
2019-11-19 13:55:34 +0400

    2

--- src/components/MainContent/Dashboard/Dashboard.js
+++ src/components/MainContent/Dashboard/Dashboard.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import css from './Dashboard.module.css'
 
-const Dashboard = () => {
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      country: []
+    };
+  }
+
+  render() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `https://restcountries.eu/rest/v2/all?fields=flag;name;language;currencies\n`, false);
+    xhr.send(JSON.stringify(xhr.responseText));
+    xhr.onreadystatechange = function() {
+      if (xhr.status != 200) {
+        this.setState({country: xhr.responseText})
+      }
+    };
+
     return (
-        <div className={css.dashboards_page}>
-            <h1>Dashboard</h1>
+      <div className={css.dashboards_page}>
+        <div>
+          <form className={css.form}>
+            <input type='text' name='country'/>
+            <button type='submit'>Find</button>
+          </form>
         </div>
+        <div>
+          <img src={this.state.country.flag} alt='error'/>
+          <p>Name: <span>{this.state.country.name}</span></p>
+          <p>Language: <span>{this.state.country.language}</span></p>
+          <p>Currency: <span>{this.state.country.currencies}</span></p>
+        </div>
+      </div>
     )
-};
+  }
+}
 
 export default Dashboard;
